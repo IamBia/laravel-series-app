f40e85c04a6315634c0c10f3d7d606494f0ce3a9
Bianca Schuurman <bia.paulaa@gmail.com>
2021-05-26 13:11:46 +0200

    added fake data to database

--- database/migrations/2021_05_26_083223_create_series_table.php
+++ database/migrations/2021_05_26_083223_create_series_table.php
@@ -14,7 +14,10 @@ class CreateSeriesTable extends Migration
     public function up()
     {
         Schema::create('series', function (Blueprint $table) {
-            $table->id();
+            $table->increments('id');
+            $table->string('name');
+            $table->integer('seasons');
+            $table->longText('description');
             $table->timestamps();
         });
     }
--- resources/views/series/index.blade.php
+++ resources/views/series/index.blade.php
@@ -2,10 +2,22 @@
 
 
 @section('content')
-<div class="m-auto w4/5 py-24">
+<div class="m-auto w-4/5 py-24">
     <div class="text-center">
         <h1 class="text-5xl bold uppercase">Series</h1>
     </div>
+    
+    <div class="w-5/6 py-10">
+    <div class="m-auto">
+        <span class="uppercase text-blue-500 font-bold text-xs italic">Seasons: 5</span>
+        
+        <h2 class="text-gray-700 text-5xl">La casa de papel</h2>
+        
+        <p class="text-lg text-gray-700 py-6">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Similique nihil tempora nulla fugit, rerum iure exercitationem assumenda voluptatem, fugiat nesciunt odit, sunt reiciendis corporis totam aspernatur asperiores dolorum eius placeat?</p>
+
+        <hr class="mt-4 mb-8">
+    </div>
+</div>
 </div>
 
 @endsection
