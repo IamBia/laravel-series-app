c03f31c422bf99f021b7a6e0f510d714dea3a192
Bianca Schuurman <bia.paulaa@gmail.com>
2021-05-26 11:54:31 +0200

    added tailwindcss

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,12 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="{{ @asset('css/app.css') }}" rel="stylesheet" >
+    <title>Series App</title>
+</head>
+<body class="bg-gradient-to-r from-gray-100 to-gray-200">
+    @yield('content')
+</body>
+</html>
---
+++ resources/views/series/index.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.app')
+
+
+@section('content')
+<div class="m-auto w4/5 py-24">
+    <div class="text-center">
+        <h1 class="text-5xl bold uppercase">Series</h1>
+    </div>
+</div>
+
+@endsection
